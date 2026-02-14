5c3b9712f60c3ba6480c1d523caa9388e1061564
Luis Alejandro Alfaro Quesada <80777677+luchodevhat@users.noreply.github.com>
2026-02-14 15:28:40 -0600

    informacion de contacto
    Se agregaron opciones e informacion de contacto directo al proveedor.

---
+++ js/whatsaap.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("whatsappForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let nombre = document.getElementById("nombre").value;
+    let mensaje = document.getElementById("mensaje").value;
+
+    let texto = `Hola, soy ${nombre}. ${mensaje}`;
+    let numero = "50670394886"; // cambia por tu número
+
+    let url = `https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
+
+    window.open(url, "_blank");
+  });
+});
