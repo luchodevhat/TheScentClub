69f47089c37908c4b282c63b6150a5ab31f51b3a
Luis Alejandro Alfaro Quesada <80777677+luchodevhat@users.noreply.github.com>
2026-02-19 14:03:00 -0600

    actualizacion inicio
    Inicio actualizado

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,25 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+
+const elements = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  elements.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+
+
+window.addEventListener('scroll', () => {
+  document.querySelector('.hero-video').classList.toggle('scrolled', window.scrollY > 50);
+});
